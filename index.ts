2bd52d3d0375fa05bdbe2b31efc6813e666f66e3
hasnain raees <hasnainraees07@gmail.com>
2024-04-18 03:14:05 +0500

    currency converter project code

---
+++ index.ts
@@ -0,0 +1,44 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let currency : any = {
+    USD:"1",
+    PKR: "278.16",
+    EURO:"0.91",
+    SAR:"3.75",
+    AED:"3.67",
+
+};
+
+
+let userAnswer = await inquirer.prompt(
+    [
+        {
+            name:"from",
+            type:"list",
+            message:"Enter Currency from",
+            choices:["USD","PKR","EURO","SAR","AED"]
+        },
+        {
+            name:"to",
+            type:"list",
+            message:"Enter Currency to",
+            choices:["USD","PKR","EURO","SAR","AED"]
+        },
+        {
+            name:"amount",
+            type:"number",
+            message:"Enter your Amount",
+        }
+    ]
+);
+
+
+let currencyFrom = currency[userAnswer.from];
+let currencyTo = currency[userAnswer.to];
+let amount = userAnswer.amount
+let baseAmount = amount / currencyFrom;
+let convertedAmount = baseAmount * currencyTo;
+
+console.log(convertedAmount);
